10806e6ecc2d209c0b93fe3b85e25d056776a449
DataArun <arunkumar.left@gmail.com>
2018-02-12 12:09:02 +0800

    Initial commit

---
+++ Currency.py
@@ -0,0 +1,205 @@
+import os
+from flask import Flask, request
+from flask.ext.jsonpify import jsonify
+import pandas as pd
+import json    
+from datetime import date, timedelta
+from flask import Response
+import datetime
+import requests_cache
+from flask.ext.cache import Cache
+from multiprocessing import Pool
+
+
+requests_cache.install_cache('demo_cache')
+app = Flask(__name__)
+app.config['CACHE_TYPE'] = 'simple'
+_pool = None
+app.cache = Cache(app)  
+
+folder = os.getcwd()+"\\Data\\"
+#folder = "C:\\Users\\Arun Kumar\\Documents\\train\\"
+currecnylist = ["CHF","EUR","GBP","OMR","BHD","KWD","SGD"]
+globCur = None
+# function for task 1 Given a date it will retrieve the currency rate of all currecny
+def task1(dateRangeA):
+        dateRangeA= dateRangeA.strip()
+        resp = ""
+        outputList = []
+        try:
+            datetime.datetime.strptime(dateRangeA, '%Y-%m-%d')
+        except:
+            resp = Response(json.dumps({"Message":"Incorrect data format, should be YYYY-MM-DD"}), status=200, mimetype='application/json')
+        if(resp==""):
+            try:
+                dateRangeA= dateRangeA
+                jsonOutput = {}
+                datafile = pd.read_csv(folder  + dateRangeA+ ".txt", sep="\t")
+                olyDate = datafile[['Currency','Amount']]
+                for Index,i in olyDate.iterrows():
+                     outputJson = {
+                             "Currency":i["Currency"],
+                             "Conversion Rate" :i["Amount"]
+                             }
+                     outputList.append(outputJson)
+                     jsonOutput  = json.dumps(outputList)
+                resp = Response(jsonOutput, status=200, mimetype='application/json')
+            except :
+                resp = Response(json.dumps({"Message":"Data not found for the date"}), status=200, mimetype='application/json')
+        return resp
+
+# function for task 2 Given a date  and two currency, it will retrieve the conversion between the two currency.
+def task2(dateRangeA,currencyA,currencyB):
+        resp = ""
+        try:
+            dateRangeA= dateRangeA
+            currencyA = currencyA
+            currencyB =  currencyB
+            dateRangeA= dateRangeA.strip()
+            currencyA = currencyA.strip().upper()
+            currencyB =  currencyB.strip().upper()
+            
+            try:
+                datetime.datetime.strptime(dateRangeA, '%Y-%m-%d')
+            except:
+                resp = Response(json.dumps({"Message":"Incorrect data format, should be YYYY-MM-DD"}), status=200, mimetype='application/json')
+            if (currencyA not in currecnylist):
+                resp = Response(json.dumps({"Message":"From Currency is not present in the list. Kindly verify the input"}), status=200, mimetype='application/json')
+            elif(currencyB not in currecnylist):
+                resp = Response(json.dumps({"Message": "To Currency is not present in the list. Kindly verify the input"}), status=200, mimetype='application/json')
+                
+            if(resp==""):  
+                try:
+                    datafile = pd.read_csv(folder  + dateRangeA+ ".txt", sep="\t")
+                    outputList = []
+                    currencyAValue =datafile[(datafile.Currency == currencyA)]["Amount"]
+                    currencyAValue = currencyAValue.iloc[0]
+                    currencyBValue =datafile[(datafile.Currency== currencyB)]["Amount"]
+                    currencyBValue = currencyBValue.iloc[0]
+                    convrsionRate = float(currencyAValue /currencyBValue) 
+                    outputJson = {
+                                "From Currency": currencyA ,
+                                "To Currency": currencyB,
+                                "Conversion Rate" : convrsionRate,
+                                "Date" : dateRangeA
+                                }
+                    outputList.append(outputJson)
+                    jsonOutput  = json.dumps(outputList)
+                    resp = Response(jsonOutput, status=200, mimetype='application/json')
+                except:
+                    
+                    resp = Response(json.dumps({"Message":"Data not found for the date"}), status=200, mimetype='application/json')
+        except:
+                resp = Response(json.dumps({"Message":"Unexpected error"}), status=200, mimetype='application/json')
+        return resp
+        
+
+# function for task 3 Given a date  range and a currency, it will retrieve the currency for the entire range.
+def task3(dateRangeA,dateRangeB,currencyA):
+        resp= ""     
+        dateRangeA= dateRangeA.strip()
+        dateRangeB= dateRangeB.strip()
+        currencyA = currencyA.strip().upper()
+        globCur= currencyA 
+        try:
+            datetime.datetime.strptime(dateRangeA, '%Y-%m-%d')
+        except:
+            resp = Response(json.dumps({"Message":"Incorrect From data format, should be YYYY-MM-DD"}), status=200, mimetype='application/json')
+        try:
+            datetime.datetime.strptime(dateRangeB, '%Y-%m-%d')
+        except:
+            resp = Response(json.dumps({"Message":"Incorrect To data format, should be YYYY-MM-DD"}), status=200, mimetype='application/json')
+        if (currencyA not in currecnylist):
+            resp = Response(json.dumps({"Message":"From Currency is not present in the list. Kindly verify the input"}), status=200, mimetype='application/json')
+
+        if(resp==""):     
+            yyyy1,mm1,dd1 = dateRangeA.split("-")
+            yyyy2,mm2,dd2 = dateRangeB.split("-")
+            
+
+            d1 = date(int(yyyy1), int(mm1), int(dd1))
+            d2 = date(int(yyyy2), int(mm2), int(dd2))
+            delta = d2 - d1
+            
+            #Enabling multiprocessing
+            p = Pool(processes = 4)    
+            actDate = []
+            for i in range(delta.days + 1):
+                actDate.append(str(d1 + timedelta(days=i)))    
+            data = [x for x in  p.map(multirange ,([(i,currencyA) for i in actDate ])) if x is not None]
+            p.close()
+            jsonOutput1  = json.dumps(data)
+            resp = Response(jsonOutput1, status=200, mimetype='application/json')
+        return resp
+
+# Task 3 's subordinate function to process the data.   
+def multirange(actDate1,currencyA):
+    jsonOutput = None
+    currencyA = "EUR"
+    outputJson= None
+    print(globCur)
+    try:
+                outputList = []
+                datafile = pd.read_csv(folder  + actDate1 + ".txt", sep="\t")
+                currencyAValue =datafile[(datafile.Currency == globCur)]["Amount"]
+                currencyAValue = currencyAValue.iloc[0]
+                outputJson = {
+                        "Date": actDate1 ,
+                        "Currency" : currencyA,
+                        "Conversion Rate" : globCur,
+                        }
+                jsonOutput  = json.dumps(outputList)
+    except:
+        1+1
+    if(jsonOutput == [] or jsonOutput == None or jsonOutput == "" ):
+        1+1
+    else:
+        return outputJson
+    
+#Error handling for api if page not found
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+
+
+@app.route('/Date/<dateRangeA>')
+@app.cache.memoize(timeout=300)
+
+# Task 1 API call with caching enabled
+def api_root1(dateRangeA):
+        r = task1(dateRangeA)
+        return r
+    
+@app.route('/CurrencyRange/<dateRangeA>/<currencyA>/<currencyB>')
+@app.cache.memoize(timeout=300)
+# Task 2 API call with caching enabled
+def api_root2(dateRangeA,currencyA,currencyB):
+        r = task2(dateRangeA,currencyA,currencyB)        
+        return r
+    
+# Task 3 API call with caching enabled
+@app.route('/Daterange/<dateRangeA>/<dateRangeB>/<currencyA>')
+@app.cache.memoize(timeout=300)
+def api_root3(dateRangeA,dateRangeB,currencyA):
+        r = task3(dateRangeA,dateRangeB,currencyA)
+        return r
+
+
+# Main function which creates the sequence of execution
+if __name__ == '__main__':
+
+    try:
+    # App runs in the path : http://127.0.0.1:5002/
+        app.run(port=5002)
+    #Stop the process of API
+    except KeyboardInterrupt:
+        _pool.close()
+        _pool.join()
---
+++ Test/test_Currency.py
@@ -0,0 +1,16 @@
+
+import sys, os
+sys.path.append('C:/Users/Arun Kumar/Documents/train')
+import Currency
+import unittest
+
+import os
+import flaskr
+import unittest
+import tempfile
+from unittest.mock import patch
+import 
+class TestCurrency(unittest.TestCase):
+    def test_task1(self):
+        Currency.task1()
+        
